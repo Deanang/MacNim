83a098d5b7249057fd61b8cd99b876fcd7e75fbe
Deanang <ngeekeng@gmail.com>
2016-06-22 04:49:45 +0800

    Add files via upload

--- source/nim.py
+++ source/nim.py
@@ -1,19 +1,3 @@
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Gist
- @Deanang
- Unwatch 1
-  Star 0
-  Fork 0 Deanang/MacNim
- Code  Issues 0  Pull requests 0  Wiki  Pulse  Graphs  Settings
-Branch: master Find file Copy pathMacNim/source/nim.py
-1b824d9  2 days ago
-@Deanang Deanang Add files via upload
-1 contributor
-RawBlameHistory     201 lines (169 sloc)  6.82 KB
 #!/usr/bin/python3
 # Nim Game by Dean Ang
 
@@ -26,18 +10,19 @@ from itertools import *
 root = Tk()
 root.wm_title('Game of Nim')
 root.wm_geometry('300x640+10+10')
-root.lift()
+root.attributes("-topmost", True)
 
 v = StringVar()
-clickedrow = "None"
+clickedrow = None
 nselected = 0
 heaps = [1,3,5,7]
 
 def hs(lst):
     return lst[0]^lst[1]^lst[2]^lst[3]
 
-def countzero(lst):
-    return sum([1 if x==0 else 0 for x in lst])
+# return True if lst1, which is shorter, is a sublist of lst2; else return False 
+def contains(lst1, lst2):
+    return True if sorted(lst1) == sorted(lst2)[:len(lst1)] else False
 
 nsum = hs(heaps)
 
@@ -57,31 +42,47 @@ class GameObject(Label):
         #Decide if image is matchstick or blank based on row number (i) and column number (j) 
         if j < heaps[i]:
             self.config(image = matchstick)
+            self.photo = matchstick
         else:
             self.config(image = blank)
+            self.photo = blank
         self.pack(side = LEFT)
+        self.clicked = 0
 
+                
         # Callback function when matchstick is clicked
         def callback(event):
-            global clickedrow, heaps, nselected
-            if j < heaps[i]:
-                if clickedrow == "None":
-                    clickedrow = i
-                if clickedrow == i:
-                    self.config(image = blank)
-                    nselected += 1
-                else:
-                    clickedrow = i
-                    nselected = 1
-                    redraw()
-                    self.config(image = blank)
 
+            global clickedrow, heaps, nselected
+
+            if j < heaps[i]:
+
+                if clickedrow == "None":
+
+                    clickedrow = i
+
+                if clickedrow == i:
+                    if self.clicked == 0:
+                        self.config(image = blank)
+
+                        nselected += 1
+                        self.clicked = 1                       
+                else:
+
+                    clickedrow = i
+
+                    nselected = 1
+
+                    redraw()
+
+                    self.config(image = blank)                   
+                    
         def commitmove(event):
             heaps[clickedrow] -= nselected
-            clickedrow = None
             nselected = 0
+            clickedrow = None
         self.bind('<Button-1>', callback)
-        self.pack(side = RIGHT)
+        self.pack(side = LEFT)
             
 #Draw 4 rows of canvases
 rows = [Canvas(root) for i in range(len(heaps))]
@@ -93,33 +94,27 @@ status = Label(root, background = 'light yellow', justify = LEFT, wraplength = 2
 comradio = Radiobutton(root, text = "Computer go first!", variable = v, value="Hello")
 title.pack(expand=1, fill=X)
 instruction.pack()
+status.pack(expand=1, fill=X)
 comradio.pack()
 
-'''
-def comstartfirst(event):
-    global heaps
-    row = randint(1,4)
-    n = randint(1, heaps[row-1])
-    heaps[row-1] -= n
-    status.config(text = 'I will start first. I haved moved ' + str(n) + ' matchstick(s) from row ' + str(row)+". Your turn now.")
-    redraw()
-    comradio.config(state="disabled")
-'''            
-status.pack(expand=1, fill=X)
+           
+
 
 #For each canvas, create a lst of Labels
 labels = [[GameObject(rows[i], i, j)  for j in range(7)]  for i in range(len(heaps))]
 for i in range(4):
     rows[i].pack()
 
-#A function to draw the matchsticks based on nsum
+#A function to draw the matchsticks based on nsumr
 def redraw():
-    global labels, heaps
+    global labels, heaps, clickedrow
     for i in range(len(heaps)):
         for j in range(heaps[i]):
             labels[i][j].config(image = matchstick)
+            labels[i][j].clicked = 0 
         for j in range(heaps[i], 7):
             labels[i][j].config(image = blank)
+            labels[i][j].clicked = 0
 
 
             
@@ -130,18 +125,11 @@ def confirmmove(event):
         status.config(text = 'Please remove at least 1 matchstick!')
     else:
         heaps[clickedrow] -= nselected
-        print("After my move, heaps is {} and nsum is {}".format(heaps, hs(heaps)))
-        if heaps == [0, 0, 0, 1] or heaps == [0, 0, 1, 0] or heaps == [0, 1, 0, 0] or heaps == [1, 0, 0, 0]:
-            heaps = [0, 0, 0, 0]
-            redraw()
-            status.config(text = 'You win! Press R to restart or Q to quit.')
-        else:
-            clickedrow = "None"
-            nselected = 0
-            status.config(text = '')
-            redraw()
-            button.config(state="disabled")
-            cmove()
+        nselected = 0
+        status.config(text = '')
+        redraw()
+        button.config(state="disabled")
+        cmove()
             
 #Create confirm move button
 button = Button(root, text = 'Confirm Move')
@@ -167,34 +155,49 @@ root.bind('<Return>', confirmmove)
 def cmove(*event):
     global heaps
     status.config(text = 'Computer to move')
+    # If player made the "correct" move, computer will randomly make the next move.
     if hs(heaps) == 0:
         row = randint(1,4)
         while(heaps[row-1] == 0):
             row = randint(1,4)
-        n = randint(1, heaps[row-1])              
+        n = randint(1, heaps[row-1])
         heaps[row-1] -= n
-        print("After computer move: heaps is {} and nsum is {}. i is {}!".format(heaps, hs(heaps),str(row)))
+        #print("After computer move: heaps is {} and nsum is {}. i is {}!".format(heaps, hs(heaps),str(row)))
         status.config(text = 'I haved moved ' + str(n) + ' matchstick(s) from row ' + str(row)+". Your turn now.")
         redraw()
         comradio.config(state="disabled")
         button.config(state="normal")
     
-    elif heaps == [0, 0, 0, 1] or heaps == [0, 0, 1, 0] or heaps == [0, 1, 0, 0] or heaps == [1, 0, 0, 0]:
+    # If player left computer the last matchstick:
+    elif contains([0,0,0,1], heaps):
         heaps = [0, 0, 0, 0]
-        redraw()
         status.config(text = 'You win! Press R to restart or Q to quit.')
-
-    elif heaps in [list(x) for x in permutations([1,2,0,0])]:
-        heaps[heaps.index(2)] = 0
         redraw()
-        status.config(text = 'I win! Press R to restart or Q to quit.')
 
-    elif countzero(heaps) == 3:
-        indx = sum([ i if x != 0 else 0 for i,x in enumerate(heaps)])
-        heaps[indx] = 1
+    #If only a row of matchsticks left, i.e 3 empty rows:
+    elif heaps.count(0) == 3:
+        n = sum([ i if x != 0 else 0 for i,x in enumerate(heaps)])
+        temp, heaps[n-1] = heaps[n-1], 1
+        status.config(text = 'I have removed {} matchsticks from row {}. I win! Press R to restart or Q to quit.'.format(temp-1, n))
         redraw()
-        status.config(text = 'I win! Press R to restart or Q to quit.') 
-        
+
+    # If heaps contains 2 empty rows and 1 row with only 1 matchstick left:
+    elif contains([0,0,1], heaps) or contains([1,1,1], heaps):
+        n = sum([ i if not (x == 0 or x == 1) else 0 for i,x in enumerate(heaps)])
+        temp, heaps[n] = heaps[n], 0
+        status.config(text = 'I have removed {} matchsticks from row {}.'.format(temp, n+1))
+        button.config(state="normal")
+        redraw()
+
+    #If heaps contains 1 empty row and 2 rows of 1 matchsticks:
+    elif contains([0,1,1], heaps):
+        n = sum([ i if not (x == 0 or x == 1) else 0 for i,x in enumerate(heaps)])
+        temp, heaps[n] = heaps[n], 1
+        button.config(state="normal")
+        status.config(text = 'I haved moved {} matchstick(s) from row {}. Your turn now.'.format(temp-1, n+1))
+        redraw()
+
+    # Algorithm for non-trivial cases not listed above
     else:
         comradio.config(state="disabled")
         nsum = hs(heaps)
@@ -203,16 +206,21 @@ def cmove(*event):
             temp = [ x if j != i else x - test for j,x in enumerate(heaps)]
             if test > 0:
                 heaps[i] -= test
-                print("After computer move: heaps is {} and nsum is {}. i is {}!".format(heaps, hs(heaps),str(i)))
-                status.config(text = 'My turn. I haved moved ' + str(nsum) + ' matchstick(s) from row ' + str(i+1))
+                #print("After computer move: heaps is {} and nsum is {}. i is {}!".format(heaps, hs(heaps),str(i)))
+                status.config(text = 'My turn. I haved moved ' + str(test) + ' matchstick(s) from row ' + str(i+1))
                 redraw()
                 button.config(state="normal")
                 break
+    if contains([0,0,0,1], heaps):
+        status.config(text = 'I win! Press R to restart or Q to quit.')
+        button.config(state="disabled")
+    if heaps == [0, 0, 0, 0]:
+        status.config(text = 'You win! Press R to restart or Q to quit.')
+        button.config(state="disabled")
+        
 
 comradio.bind('<Button-1>', cmove)
 
 root.mainloop()
 
 
-Status API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security Contact Help
